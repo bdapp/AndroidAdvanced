80631b3ab5e44a1e2046f3cf7da39e4aa04d4785
bello <belloving@gmail.com>
2019-03-10 23:54:35 +0800

    motionEvent

---
+++ A1MotionEvent/app/src/main/java/me/bello/a1motionevent/MainActivity.java
@@ -0,0 +1,22 @@
+package me.bello.a1motionevent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.my_text).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
---
+++ A1MotionEvent/app/src/main/java/me/bello/a1motionevent/MyTextView.java
@@ -0,0 +1,69 @@
+package me.bello.a1motionevent;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.TextView;
+
+/**
+ * @Info
+ * @Auth Bello
+ * @Time 2019/3/10 23:39
+ * @Ver
+ */
+public class MyTextView extends TextView {
+    private static final String TAG = MyTextView.class.getSimpleName();
+
+    public MyTextView(Context context) {
+        super(context);
+    }
+
+    public MyTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    //分发
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                Log.e(TAG, "dispatchTouchEvent ACTION_DOWN");
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                Log.e(TAG, "dispatchTouchEvent ACTION_MOVE");
+                break;
+
+            case MotionEvent.ACTION_UP:
+                Log.e(TAG, "dispatchTouchEvent ACTION_UP");
+                break;
+        }
+        return super.dispatchTouchEvent(event);
+    }
+
+    // 消费
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                Log.e(TAG, "onTouchEvent ACTION_DOWN");
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                Log.e(TAG, "onTouchEvent ACTION_MOVE");
+                break;
+
+            case MotionEvent.ACTION_UP:
+                Log.e(TAG, "onTouchEvent ACTION_UP");
+                break;
+        }
+        return super.onTouchEvent(event);
+    }
+
+}
